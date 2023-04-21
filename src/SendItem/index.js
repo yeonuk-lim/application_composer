72f5aa1d19864ff28dea5a38d36cc6a527105223
yeonuk.lim <lim454@naver.com>
2023-04-22 03:46:18 +0900

    update SendItem

--- src/SendItem/index.js
+++ src/SendItem/index.js
@@ -1,13 +1,12 @@
 // Load the AWS SDK for Node.js
 const AWS = require('aws-sdk');
-// Create the DynamoDB service object
 AWS.config.update({region: 'us-east-1'});
 
 var sns = new AWS.SNS({apiVersion: '2012-11-05'})
 
 exports.handler = async event => {
   var params = {
-      Message: JSON.parse(event.body).body,
+      Message: JSON.parse(event.body).message,
       Subject: "Special Price",
       TopicArn: process.env.TOPIC_ARN
   };
